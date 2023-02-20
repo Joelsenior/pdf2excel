201f8df3ae82c2b8d99ec426fd14c70814d3ea07
JOel Maisel <1279767778@qq.com>
2023-02-20 17:37:20 +0800

    修改了xmlparse.py

--- word2excel/xmlparse.py
+++ word2excel/xmlparse.py
@@ -4,6 +4,14 @@ root = xp.documentElement
 bodys = root.getElementsByTagName("w:body")
 body = bodys[0]
 for i,ele in enumerate(body.childNodes):
-    e_name = ele.nodeName
-    print(i,"->",e_name,"is",ele)
+    # e_name = ele.nodeName
+    # print(i,"->",e_name,"is",ele)
+    #找到包含w:t的标签，可能是多个
+    wts = ele.getElementsByTagName("w:t")
+    ele_text = "" #记录大单元内所有文本
+    for wt in wts:   
+        ele_text = ele_text + wt.text
+    print(ele_text)
+
+
 
